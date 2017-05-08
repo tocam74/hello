daf7b4b91b48203ffcca17dcffe3864fe42750b3
Tomislav Mitrovic <tomislav.mitrovic@gmail.com>
2017-05-08 13:47:27 +0200

    Registracija

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete
+)
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
@@ -24,5 +30,10 @@ urlpatterns = [
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^loonatics/(?P<slug>[-\w]+)/$', views.loonatic_detail, name='loonatic_detail'),
     url(r'^loonatics/(?P<slug>[-\w]+)/edit/$', views.edit_loonatic, name='edit_loonatic'),
+    url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
+    url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9a-zA-Z]+)-(?P<token>.+)/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
+    url(r'^accounts/password/done/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name="password_reset_complete"),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
