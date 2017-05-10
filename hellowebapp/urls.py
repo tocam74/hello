040f9247cec2c06e0dd6642cd1a12a34c7529c62
Tomislav Mitrovic <tomislav.mitrovic@gmail.com>
2017-05-10 09:47:35 +0200

    promena pasvorda

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -21,7 +21,9 @@ from django.contrib.auth.views import (
     password_reset,
     password_reset_done,
     password_reset_confirm,
-    password_reset_complete
+    password_reset_complete,
+    password_change,
+    password_change_done
 )
 from collection.backends import MyRegistrationView
 
@@ -31,6 +33,8 @@ urlpatterns = [
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^loonatics/(?P<slug>[-\w]+)/$', views.loonatic_detail, name='loonatic_detail'),
     url(r'^loonatics/(?P<slug>[-\w]+)/edit/$', views.edit_loonatic, name='edit_loonatic'),
+    url(r'^loonatics/password/change/$', password_change, {'template_name': 'registration/password_change_form.html'}, name="password_change"),
+    url(r'^loonatics/password/change/done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}, name="password_change_done"),
     url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
     url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
     url(r'^accounts/password/reset/(?P<uidb64>[0-9a-zA-Z]+)-(?P<token>.+)/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
