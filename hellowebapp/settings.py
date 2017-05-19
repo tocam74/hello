33d0c932a37ba3fb1c99da28272f2c22d93248b1
Tomislav Mitrovic <tomislav.mitrovic@gmail.com>
2017-05-19 11:25:15 +0200

    Tests

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yyut&ts3k-1c)smk=+q=9sraog6@ae8zqbc8cfgc0rxzf9pdv#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
